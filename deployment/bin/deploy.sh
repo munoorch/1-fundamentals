760765e3200a65b083a8633e63a37d7d1fb97f1d
munoorch <munoorch@gmail.com>
2023-11-27 20:29:54 +0300

    replace martin mhd

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -15,9 +15,9 @@ git config --global --add safe.directory $PROJECT_DIR
 
 # the project has not been cloned yet (first deploy)
 if [ ! -d $PROJECT_DIR"/.git" ]; then
-  GIT_SSH_COMMAND='ssh -i /home/martin/.ssh/id_rsa -o IdentitiesOnly=yes' git clone git@github.com:mmartinjoo/devops-with-laravel-sample.git .
+  GIT_SSH_COMMAND='ssh -i /home/mhd/.ssh/id_rsa -o IdentitiesOnly=yes' git clone https://github.com/munoorch/1-fundamentals .
 else
-  GIT_SSH_COMMAND='ssh -i /home/martin/.ssh/id_rsa -o IdentitiesOnly=yes' git pull
+  GIT_SSH_COMMAND='ssh -i /home/mhd/.ssh/id_rsa -o IdentitiesOnly=yes' git pull
 fi
 
 cd $PROJECT_DIR"/frontend"
--- deployment/bin/provision_server/provision_server.sh
+++ deployment/bin/provision_server/provision_server.sh
@@ -16,7 +16,7 @@ cd $PROJECT_DIR
 git config --global --add safe.directory $PROJECT_DIR
 
 if [ ! -d $PROJECT_DIR"/.git" ]; then
-  GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes' git clone git@github.com:mmartinjoo/devops-with-laravel-sample.git .
+  GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes' git clone git@github.com:munoorch/1-fundamentals.git .
   cp $PROJECT_DIR"/api/.env.example" $PROJECT_DIR"/api/.env"
   sed -i "/DB_PASSWORD/c\DB_PASSWORD=$MYSQL_PASSWORD" $PROJECT_DIR"/api/.env"
   sed -i '/QUEUE_CONNECTION/c\QUEUE_CONNECTION=database' $PROJECT_DIR"/api/.env"
@@ -73,17 +73,17 @@ unzip awscliv2.zip
 
 aws configure
 
-useradd -G www-data,root -u 1000 -d /home/martin martin
-mkdir -p /home/martin/.ssh
-touch /home/martin/.ssh/authorized_keys
-chown -R martin:martin /home/martin
-chown -R martin:martin /var/www/html
-chmod 700 /home/martin/.ssh
-chmod 644 /home/martin/.ssh/authorized_keys
+useradd -G www-data,root -u 1000 -d /home/mhd mhd
+mkdir -p /home/mhd/.ssh
+touch /home/mhd/.ssh/authorized_keys
+chown -R mhd:mhd /home/mhd
+chown -R mhd:mhd /var/www/html
+chmod 700 /home/mhd/.ssh
+chmod 644 /home/mhd/.ssh/authorized_keys
 
-echo "$SSH_KEY" >> /home/martin/.ssh/authorized_keys
+echo "$SSH_KEY" >> /home/mhd/.ssh/authorized_keys
 
-echo "martin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/martin
+echo "mhd ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/mhd
 
 php -v
 node -v
