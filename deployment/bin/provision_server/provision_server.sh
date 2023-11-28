c5612188c5e9d2e59e08c00607d05426112f53db
munoorch <munoorch@gmail.com>
2023-11-28 14:30:42 +0300

    add sudo

--- deployment/bin/provision_server/provision_server.sh
+++ deployment/bin/provision_server/provision_server.sh
@@ -7,16 +7,16 @@ SSH_KEY=$2
 
 PROJECT_DIR="/var/www/html/posts"
 
-mkdir -p $PROJECT_DIR
+sudo mkdir -p $PROJECT_DIR
 
-chown -R www-data:www-data $PROJECT_DIR
+sudo chown -R www-data:www-data $PROJECT_DIR
 
 cd $PROJECT_DIR
 
 git config --global --add safe.directory $PROJECT_DIR
 
 if [ ! -d $PROJECT_DIR"/.git" ]; then
-  GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes' git clone git@github.com:munoorch/1-fundamentals.git .
+  GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa -o IdentitiesOnly=yes' sudo git clone git@github.com:munoorch/1-fundamentals.git .
   cp $PROJECT_DIR"/api/.env.example" $PROJECT_DIR"/api/.env"
   sed -i "/DB_PASSWORD/c\DB_PASSWORD=$MYSQL_PASSWORD" $PROJECT_DIR"/api/.env"
   sed -i '/QUEUE_CONNECTION/c\QUEUE_CONNECTION=database' $PROJECT_DIR"/api/.env"
--- deployment/bin/provision_server/run_provision_server_from_local.sh
+++ deployment/bin/provision_server/run_provision_server_from_local.sh
@@ -9,8 +9,8 @@ MYSQL_PASSWORD=$3
 PUBLIC_SSH_KEY=$(cat $HOME/.ssh/id_rsa.pub)
 
 scp -C -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa $HOME/.ssh/id_rsa $SSH_USER@$SERVER_IP:~/.ssh/id_rsa
-ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa $SSH_USER@$SERVER_IP "ssh-keyscan github.com >> ~/.ssh/known_hosts"
+ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa $SSH_USER@$SERVER_IP "sudo ssh-keyscan github.com >> ~/.ssh/known_hosts"
 
 scp -C -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa ./provision_server.sh $SSH_USER@$SERVER_IP:./provision_server.sh
-ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa $SSH_USER@$SERVER_IP "chmod +x ./provision_server.sh && ./provision_server.sh $MYSQL_PASSWORD \"$PUBLIC_SSH_KEY\""
-ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa $SSH_USER@$SERVER_IP "ssh-keyscan github.com >> /home/mhd/.ssh/known_hosts"
+ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa $SSH_USER@$SERVER_IP "sudo chmod +x ./provision_server.sh && ./provision_server.sh $MYSQL_PASSWORD \"$PUBLIC_SSH_KEY\""
+ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_rsa $SSH_USER@$SERVER_IP "sudo ssh-keyscan github.com >> /home/mhd/.ssh/known_hosts"
